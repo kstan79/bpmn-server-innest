5874b1d61ab66139b9ea28c7680e0c383b7fae39
kstan <kstan@simitgroup.com>
2024-03-16 09:34:04 +0800

    add blank repo

---
+++ src/simpleapp/generate/sharelibs/money.ts
@@ -0,0 +1,15 @@
+export const getSubtotal = (details) => {
+  details.subTotal = details.unitPrice * details.quantity;
+  return details;
+};
+
+export const getTotal = (details: any[]) => {
+  if (details && details.length > 0) {
+    let total = 0;
+    details.forEach((item) => {
+      total += item.subTotal;
+    });
+    return total;
+  }
+  return 0;
+};
---
+++ src/simpleapp/generate/types/index.ts
@@ -0,0 +1,25 @@
+export * from './tenant.type';
+
+export * from './org.type';
+
+export * from './branch.type';
+
+export * from './perm.type';
+
+export * from './autoinc.type';
+
+export * from './docno.type';
+
+export * from './cat.type';
+
+export * from './cust.type';
+
+export * from './inv.type';
+
+export * from './prd.type';
+
+export * from './user.type';
+export * from './simpleapp.type';
+export * from './workflow.type';
+
+export * from '../../types';
